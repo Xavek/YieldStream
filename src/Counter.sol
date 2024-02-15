08b1ba5ca27abb21cff2eca6738fe94936d8c511
Xavek <theofficalgautam200@gmail.com>
2024-02-15 18:28:06 +0545

    init commit

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+struct StreamDetails{
+    address to;
+    uint256 amount;
+    uint256 flow_rate;
+    uint256 startTime;
+    address sender;
+    bool is_active;
+    address erc20_account_address;
+}
+
 contract Counter {
-    uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
+   function create_stream(address erc20_contract_address,address to, uint256 amount,uint256 flow_rate) public {
+    // check the balance of the erc20 address this caller wants to stream
+    // transfer that amount to this contract and 
+    // trigger blast transfer
+    // that's it
+    // need to add oz
+   } 
 }
